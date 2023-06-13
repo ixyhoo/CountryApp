e107bbf11e5d10da06aa3abe222bb08102c5008a
ixyhoo <ixyhoo@outlook.fr>
2023-06-13 10:45:52 +0200

    continue later

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
-const buttonSortName = document.getElementById("minToMax");
-const buttonSortPopulation = document.getElementById("maxToMin");
-const buttonSortAlpha = document.getElementById("alpha");
 
-let countries = [];
+let contriesData = [];
 
-async function fetchCountries() {
-  await fetch("https://restcountries.com/v3.1/all")
-    .then((response) => response.json())
-    .then((data) => {
-      countries = data.countries;
-      console.log(countries);
-    });
-}
-
-if (buttonSortName) {
-    buttonSortName.addEventListener("click", () => {
-      fetchCountries();
-    });
-  }
-});
+  fetch("https://restcountries.com/v3.1/all")
+  .then((res)=>res.json())
+  .then((data)=>console.log(data));
+ 
 
 
 // 2 - Créer une fonction pour "fetcher" les données, afficher les données dans la console.
@@ -29,7 +14,7 @@ if (buttonSortName) {
 // 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
 
 // 5 - Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
-country.name.includes(inputSearch.value);
+//country.name.includes(inputSearch.value);
 
 // 6 - Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
 
