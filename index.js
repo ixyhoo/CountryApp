6ceadb0a421ae664c70a89f5aa52166f6d2b6dea
ixyhoo <ixyhoo@outlook.fr>
2023-06-13 09:58:46 +0200

    fetch

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-// 1 - Tester le lien de l'API dans le navigateur (https://restcountries.com/v3.1/all)
+
+const buttonSortName = document.getElementById("minToMax");
+const buttonSortPopulation = document.getElementById("maxToMin"); 
+const buttonSortAplha = document.getElementById("alpha"); 
+const inputSearch = document.getElementById("search");
+const inputRange = document.getElementById("range");
+
+let countries = [];
+
+async function fetchCountries(search) {
+
+   await fetch("https://restcountries.com/v3.1/all")
+    .then((response) => response.json())
+        .then((data) => (countries = data.countries))
+            console.log(countries);
+        
+    
+}
+
 
 // 2 - Créer une fonction pour "fetcher" les données, afficher les données dans la console.
 
@@ -7,7 +25,7 @@
 // 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
 
 // 5 - Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
-coutry.name.includes(inputSearch.value);
+country.name.includes(inputSearch.value);
 
 // 6 - Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
 
